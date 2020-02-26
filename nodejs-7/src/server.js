5d079c6117a73af1475be21c9a161bb2ac61f48b
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-02-26 10:52:00 -0300

    CRUD implemented

--- nodejs-7/src/server.js
+++ nodejs-7/src/server.js
@@ -7,24 +7,66 @@ const { animals } = require('./model/')
 app.use(bodyParser.json())
 
 app.get('/v1/animals', async (req, res, next) => {
-  res.status(501).send('Not Implemented')
+  try {
+    const data = await animals.findAll({})
+    res.status(200).json({
+      total: data.length,
+      data
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.get('/v1/animals/:animalId', async (req, res, next) => {
-  res.status(501).send('Not Implemented')
+  try {
+    const { animalId } = req.params
+    const data = await animals.findOne({
+    where: { id: animalId }
+  })
+  res.status(200).json(data)
+  } catch (err) {
+    console.log(err) 
+  }
 })
 
 app.post('/v1/animals', async (req, res, next) => {
-  const data = animals.create(req.body)
-  res.status(201).json(data)
+
+  try {
+    const data = await animals.create(req.body)
+    res.status(201).json(data)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.patch('/v1/animals/:animalId', async (req, res, next) => {
-  res.status(501).send('Not Implemented')
+  try {
+    const { animalId } = req.params
+    const bodyUpdate = req.body
+    await animals.update(bodyUpdate, {
+      where: { 
+        id: animalId
+      }
+    })
+    res.status(200).json({})
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 app.delete('/v1/animals/:animalId', async (req, res, next) => {
-  res.status(501).send('Not Implemented')
+  try {
+    const { animalId } = req.params
+    const data = await animals.destroy({
+    where: { 
+      id: animalId
+    }
+  })
+  res.status(204).json(data)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 const start = async (port = 8080) => {
