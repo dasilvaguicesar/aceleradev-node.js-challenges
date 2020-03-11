5414857c7905c2ce6db1eef181d8a6e068a29c87
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-03-11 11:26:05 -0300

    AAPI with JWT added

---
+++ teams-api-jwt/src/controllers/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const { auth } = require('../config')
+
+let Auth = {}
+
+Auth.getToken = async (req, res, next) => {
+  const { body: { user, password } } = req
+
+  if (auth.user === user && auth.password === password) {
+    
+    const token = jwt.sign( { user }, auth.secret, { expiresIn: '1h' })
+    res.status(200).json({"token": token})
+  } else {
+    res.status(401).json({
+      "error": "Invalid user or password."})
+  }
+}
+
+module.exports = Auth
---
+++ teams-api-jwt/src/middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+const { auth } = require('../config')
+
+module.exports = (req, res, next) => {
+    const token = req.get('X-Auth-Token')
+    jwt.verify(token, auth.secret, (err, decoded) => {
+      if(err){
+        res.status(401).json({
+          "error": "Invalid token."
+        })
+      } else {
+        next()
+      }
+    })
+  }
